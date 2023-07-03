20ff53d6c22c59f73ee62a9c9970f068d19649f6
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2023-07-03 15:35:15 +0530

    done

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; 
-server.use("cors")
+const port = process.env.PORT || 3001;
+
 server.use(middlewares);
 server.use(router);
 
